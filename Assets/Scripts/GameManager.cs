96693161948d6b2dbd56db6f7374f078afe60825
SJ <86448785+SamerJurdi@users.noreply.github.com>
2024-12-06 18:07:53 +0300

    added test field in GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     public string nameOfSomethihg;
     public GameObject Player;
+    public string thisIsATest;
     // Start is called before the first frame update
     void Start()
     {
