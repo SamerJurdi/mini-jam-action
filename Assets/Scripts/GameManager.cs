20e4e371179d3432c942a479cf4b2355db4ae04e
Catwingzz <burbachdrew@gmail.com>
2024-12-06 10:11:57 -0500

    Adding a new public string to gamemanager for "JustTesting"

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
     public string nameOfSomethihg;
     public GameObject Player;
     public string thisIsATest;
+    public string JustTesting;
     // Start is called before the first frame update
     void Start()
     {
