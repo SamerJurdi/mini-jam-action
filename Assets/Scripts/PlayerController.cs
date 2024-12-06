cd09233f0e298a1dd8c9a7199a846041aa2f6e5b
SJ <86448785+SamerJurdi@users.noreply.github.com>
2024-12-06 19:56:34 +0300

    added player boundaries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Player Movement")]
     public float moveSpeed = 5f;
     public float tiltAngle = 45f;
     private Rigidbody2D rb;
     private Vector2 movement;
 
+    [Header("Boundaries Configuration")]
+    [Range(0f, 1f)]
+    public float maxHeight = 0.3f;
+    private float screenHeight;
+    private float minY;
+    private float maxY;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        // Calculate screen boundaries
+        screenHeight = Camera.main.orthographicSize * 2f;
+        minY = -screenHeight / 2;
+        maxY = minY + screenHeight * maxHeight;
     }
 
     void Update()
@@ -22,6 +35,7 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         UpdateMovement();
+        EnforceBoundary();
     }
 
     private void UpdateMovement()
@@ -30,6 +44,7 @@ public class PlayerController : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");
         rb.velocity = movement.normalized * moveSpeed;
     }
+
     private void UpdateRotation(float horizontalMovement)
     {
         if (horizontalMovement < 0)
@@ -45,5 +60,11 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
-}
 
+    private void EnforceBoundary()
+    {
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, minY, maxY);
+        transform.position = clampedPosition;
+    }
+}
