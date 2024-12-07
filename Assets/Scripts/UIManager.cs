050ade9ed63cf4150465bfd63f1ec4adb1cca7ec
SJ <86448785+SamerJurdi@users.noreply.github.com>
2024-12-07 20:22:20 +0300

    customized the UI to look like the mockup provided by artist

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,27 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     // The only thing this class does, is looks at components and updates the UI
     [Header("Health")]
-    public Image healthbar;
+    public GameObject healthbar;
+    private float hbMaxWidth;
+    private RectTransform hbTransform;
     [Header("Score")]
     public TextMeshProUGUI score;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hbTransform = healthbar.GetComponent<RectTransform>();
+        hbMaxWidth = hbTransform.rect.width;
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthbar.fillAmount = GameManager.GM.GetEarthHealth();
+        Vector2 hbCurrentSize = hbTransform.sizeDelta;
+        hbCurrentSize.x = hbMaxWidth * GameManager.GM.GetEarthHealth();
+        hbTransform.sizeDelta = hbCurrentSize;
         score.text = "" + GameManager.GM.Score;
     }
 }
