09007291fcf546d889dc3142b4c9a07e2aab5a3a
SJ <86448785+SamerJurdi@users.noreply.github.com>
2024-12-07 14:04:11 +0300

    refactored the main menu components into UI components to improve handling

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,19 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public GameObject StartButton;
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        //SpriteRenderer startSprite = StartButton.GetComponent<SpriteRenderer>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("GameScene");
     }
 }
