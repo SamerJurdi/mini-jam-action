3227dc6b67cd8020f5652a3c64f1dc4a83a03138
SJ <86448785+SamerJurdi@users.noreply.github.com>
2024-12-08 11:24:56 +0300

    replaced animation script with proper animation components in the main menu and gave the animation some bounce

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -3,17 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public GameObject GameTitle;
     public float titleMoveSpeed;
     public float titleFinalPosY;
-
-    void Update()
-    {
-        if(GameTitle.transform.position.y > titleFinalPosY)
-        {
-            GameTitle.transform.position = new Vector3(GameTitle.transform.position.x, GameTitle.transform.position.y - (titleMoveSpeed * Time.deltaTime), GameTitle.transform.position.z);
-        }
-    }
 
     public void PlayGame()
     {
